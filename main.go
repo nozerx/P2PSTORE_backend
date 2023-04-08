7c3f3d67671f80c049da6a88f07976e19643e676
nozerx <104958959+nozerx@users.noreply.github.com>
2023-04-08 23:30:07 +0530

    documentation commit

--- main.go
+++ main.go
@@ -8,11 +8,15 @@ import (
 )
 
 func main() {
+
+	// The following code calls the fuction InitFolders() to initialize all the core folders essential for the node to properly work
 	err := initnode.InitFolders()
 	if err != nil {
+		// In case of any errors while creation of folders, node cannot function properly, so has to shut down
 		fmt.Println(err.Error())
 		fmt.Println("[WARNING] - node will exit in 10 seconds")
 		time.Sleep(10 * time.Second)
+		return
 	}
 	_, _, err = p2p.EstablishP2PNode()
 	if err != nil {
